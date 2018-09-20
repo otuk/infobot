daecfd8bd1534d401142d7c3d8d69cc04dad6a34
Otuk Keelson <otuk@kodeten.com>
2018-09-20 10:51:02 -0400

    setup.py package list updated

--- setup.py
+++ setup.py
@@ -43,7 +43,8 @@ setup(
     include_package_data=True,
     keywords='infobot',
     name='infobot',
-    packages=find_packages(include=['infobot']),
+    packages=find_packages(
+        include=['infobot', 'infobot.storage', 'infobot.social']),
     setup_requires=setup_requirements,
     test_suite='tests',
     tests_require=test_requirements,
